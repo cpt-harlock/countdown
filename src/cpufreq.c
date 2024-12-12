63ed3b598ac42fc5cc96726c0b6cab6b6de5aa42
cpt-harlock <andrea.monterubbiano@gmail.com>
2024-12-12 16:21:02 +0100

    rdonly for max/min freq file

--- src/cpufreq.c
+++ src/cpufreq.c
@@ -41,12 +41,14 @@ HIDDEN void init_cpufreq() {
              STRING_SIZE     ,
              SCALING_MAX_FREQ,
              cntd->rank->cpu_id);
-	cntd->policy_limits_freq_fd[2] = open_file(filename, O_RDWR);
+	//cntd->policy_limits_freq_fd[2] = open_file(filename, O_RDWR);
+	cntd->policy_limits_freq_fd[2] = open_file(filename, O_RDONLY);
 	snprintf(filename        ,
              STRING_SIZE     ,
              SCALING_MIN_FREQ,
              cntd->rank->cpu_id);
-	cntd->policy_limits_freq_fd[3] = open_file(filename, O_RDWR);
+	//cntd->policy_limits_freq_fd[3] = open_file(filename, O_RDWR);
+	cntd->policy_limits_freq_fd[3] = open_file(filename, O_RDONLY);
 
 	int scaling_setspeed_flags = O_RDONLY;
 	if (cntd->userspace_governor)
